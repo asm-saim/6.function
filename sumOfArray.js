086d3d919caf4243e2b240fa2e7db05b878ce06f
asm-saim <saim777m@gmail.com>
2026-07-24 19:24:55 +0600

    given multiple array to get sum using only one function

--- sumOfArray.js
+++ sumOfArray.js
@@ -1,6 +1,9 @@
-//sum of array:
+//sum of array using function: power of reusability
+// we may use for of loop to get sum, but it will take multiple loop for multiple array. 
 
-const numbers = [3, 55, 77, 89, 33, 56, 88]
+const numbers = [3, 55, 77, 89, 33, 56, 88];
+const numbers2 = [45, 4, 24, 80, 336, 899];
+const numbers3 = [455, 6, 46, 55, 99];
 
 function arraySum(array) {
     let sum = 0;
@@ -11,4 +14,10 @@ function arraySum(array) {
     return sum;
 }
 const result = arraySum(numbers);
-console.log(result)
+console.log(result);
+
+const result2 = arraySum(numbers2);
+console.log(result2);
+
+const result3 = arraySum(numbers3);
+console.log(result3)
