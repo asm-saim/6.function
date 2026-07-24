56c47504b51c741e19aaca10ebe9ce7c6be15817
asm-saim <saim777m@gmail.com>
2026-07-24 11:12:56 +0600

    arguments and parameter explained

--- function.js
+++ function.js
@@ -12,3 +12,12 @@ function calculator(a, b) {
 // calculator(10,10)
 const res = calculator(5, 5)
 console.log(res)
+
+
+//multiplication:
+function multiply(a,b){ //parameter
+    const multiply=a*b;
+    return multiply;
+}
+const result=multiply(2,4,5,7,8)// argument 
+console.log(result)
