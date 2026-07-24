f2a184396c1e1dc0926443207488601cae2bdfea
asm-saim <saim777m@gmail.com>
2026-07-24 09:11:42 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,14 @@
+//function layout:
+
+// function morningTask(){
+
+// }
+
+function calculator(a, b) {
+    const sum = a + b;
+    return sum
+}
+
+// calculator(10,10)
+const res = calculator(5, 5)
+console.log(res)
