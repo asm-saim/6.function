7ad601759defc53f6d11a7f3b394658e9b0bd601
asm-saim <saim777m@gmail.com>
2026-07-24 21:45:30 +0600

    get even array

---
+++ evenNumbersArray.js
@@ -0,0 +1,13 @@
+const numbers = [23, 66, 89, 33, 23, 68, 32]
+
+function evenArray(even) {
+    let evenNumbers = [];
+    for (number of even) {
+        if (number % 2 === 0) {
+            evenNumbers.push(number)
+        }
+    }
+    return evenNumbers;
+}
+const result = evenArray(numbers);
+console.log(result);
