26c110edeea77aa43333e8d7822d8ec94b28909d
asm-saim <saim777m@gmail.com>
2026-07-24 19:16:57 +0600

    sum of array using function

---
+++ sumOfArray.js
@@ -0,0 +1,14 @@
+//sum of array:
+
+const numbers = [3, 55, 77, 89, 33, 56, 88]
+
+function arraySum(array) {
+    let sum = 0;
+    for (number of array) {
+        sum = sum + number;
+    }
+
+    return sum;
+}
+const result = arraySum(numbers);
+console.log(result)
